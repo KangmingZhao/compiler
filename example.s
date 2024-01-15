98305a85afafec1c3386e7489de8fdd0ede88c25
傅桐 <704182107@qq.com>
2024-01-15 22:20:17 +0800

    test6

--- example.s
+++ example.s
@@ -1,27 +1,45 @@
 	.arch armv8-a
 	.arch_extension crc
 	.arm
+.data
+.global a
+a:
+	.word 0
 	.text
-	.global fuck
-	.type fuck , %function
-fuck:
-	push {r4, fp, lr}
+	.global func
+	.type func , %function
+func:
+	push {r4, r5, fp, lr}
 	mov fp, sp
-	sub sp, sp, #16
-	str r0, [sp, #-16]
-	str r1, [sp, #-12]
-	str r2, [sp, #-8]
-	str r3, [sp, #-4]
-.L10:
-	ldr r4, =0
-	str r4, [fp, #-16]
-	ldr r4, =1
-	str r4, [fp, #-12]
-	ldr r4, =2
-	str r4, [fp, #-8]
-	ldr r4, =3
+	sub sp, sp, #4
+	str r0, [sp, #-4]
+.L13:
+	ldr r4, [fp, #-4]
+	sub r5, r4, #1
+	str r5, [fp, #-4]
+	ldr r4, [fp, #-4]
+	mov r0, r4
+	add sp, sp, #4
+	pop {r4, r5, fp, lr }
+	bx lr
+	.global main
+	.type main , %function
+main:
+	push {r4, r5, fp, lr}
+	mov fp, sp
+	sub sp, sp, #4
+.L16:
+	ldr r4, =10
+	ldr r5, =a
+	str r4, [r5]
+	ldr r4, =a
+	ldr r5, [r4]
+	mov r0, r5
+	bl func
+	mov r4, r0
 	str r4, [fp, #-4]
-	ldr r4, =4
-	add sp, sp, #16
-	pop {r4, fp, lr }
+	ldr r4, [fp, #-4]
+	mov r0, r4
+	add sp, sp, #4
+	pop {r4, r5, fp, lr }
 	bx lr
--- src/Ast.cpp
+++ src/Ast.cpp
@@ -169,7 +169,7 @@ void FunctionDef::genCode()
     }
 
     //for (int i = (int)ParaNodeStack[ParaNodeStack.size() - 1].size() - 1; i >= 0; i--)
-    for (int i = 0; i < (int)ParaNodeStack[ParaNodeStack.size() - 1].size() - 1; i++)
+    for (int i = 0; i < (int)ParaNodeStack[ParaNodeStack.size() - 1].size(); i++)
     {
         //std::cout << ParaNodeStack[ParaNodeStack.size() - 1][i].se->toStr() << std::endl;
         Operand* addr = ParaNodeStack[ParaNodeStack.size() - 1][i].addr;
